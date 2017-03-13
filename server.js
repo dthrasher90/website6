49de9568e5b028be792dd6c658e123981ac860a6
dthrasher90 <dthrasher90@GMAIL.COM>
2017-03-13 15:55:53 -0800

    inital
    initial

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+app.get('/', function (req, res){
+  console.log("hello world");
+});
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000");
+});
